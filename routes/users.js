fdf7507b4177b6eab3d3c1786b8afa6e761c7c95
Sharon Reshef <sharonreshef89@gmail.com>
2019-06-01 16:44:44 +0300

    initial commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql2/promise');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+let pool;
+(async function initializePool() {
+    pool = await mysql.createPool({
+        host: 'localhost',
+        user: 'root',
+        password: '12345678',
+        database: 'mynextvacation',
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0,
+    });
+})();
+
+router.get('/', async (req, res) => {
+  console.log('test', req.test);
+  const [results, fields] = await pool.execute(`SELECT * FROM users`);
+  // line 13 is equivalent to this:
+  // const arr = await connection.execute(`SELECT * FROM Movies`);
+  // const results = arr[0];
+  // const fields = arr[1];
+
+  res.send(results);
 });
 
 module.exports = router;
