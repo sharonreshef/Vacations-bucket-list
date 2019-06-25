a5624ac005cc0e08d9504caba8bb691a8309b8b3
Sharon Reshef <sharonreshef89@gmail.com>
2019-06-25 23:57:29 +0300

    setup react router

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
-    <section class='landing'>
-      <div class='dark-overlay'>
-        <div class='landing-inner'>
-          <h1 class='x-large'>My Vacation Bucketlist</h1>
-          <p class='lead'>
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>My Vacation Bucketlist</h1>
+          <p className='lead'>
             Save your desire bucketlist vacations to see and be updated the best
             prices and dates avilable{' '}
           </p>
-          <div class='buttons'>
-            <a href='register.html' class='btn btn-primary'>
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>
               Sign Up
-            </a>
-            <a href='login.html' class='btn btn'>
+            </Link>
+            <Link to='/login' className='btn btn'>
               Login
-            </a>
+            </Link>
           </div>
         </div>
       </div>
