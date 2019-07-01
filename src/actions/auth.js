48021b2179ecf289370182ca140717a1bb184f3d
Sharon Reshef <sharonreshef89@gmail.com>
2019-07-02 00:15:29 +0300

    Logout user and change navbar links

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -5,7 +5,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from './types';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
@@ -98,4 +99,11 @@ export const login = (email, userPassword) => async dispatch => {
       type: LOGIN_FAIL
     });
   }
+};
+
+// Logout
+export const logout = () => dispatch => {
+  dispatch({
+    type: LOGOUT
+  });
 };
